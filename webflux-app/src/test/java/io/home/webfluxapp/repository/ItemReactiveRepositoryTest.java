6b6f1abd226cfd1ce9e5c4d41cdd261fe9905af7
Vipul Kumar Sharma <tysonvks@gmail.com>
2020-03-10 01:22:57 +0530

    @DirtiesContext

--- webflux-app/src/test/java/io/home/webfluxapp/repository/ItemReactiveRepositoryTest.java
+++ webflux-app/src/test/java/io/home/webfluxapp/repository/ItemReactiveRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @DataR2dbcTest
 @RunWith(SpringRunner.class)
+@DirtiesContext
 public class ItemReactiveRepositoryTest {
 
     @Autowired
