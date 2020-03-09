40b705c48a08393e2070634c560d3c81b33e0815
Vipul Kumar Sharma <tysonvks@gmail.com>
2020-03-09 23:37:00 +0530

    Added Reactive h2-DB

---
+++ webflux-app/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO EMPLOYEE (id, name, salary) VALUES ('1', 'Ajay', '56745');
+INSERT INTO EMPLOYEE (id, name, salary) VALUES ('2', 'Vipul', '31345');
+INSERT INTO EMPLOYEE (id, name, salary) VALUES ('3', 'Rakesh', '123531');
+INSERT INTO EMPLOYEE (id, name, salary) VALUES ('4', 'Abhinash', '123531');
